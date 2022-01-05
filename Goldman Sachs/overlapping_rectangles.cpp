39fca27fbb655d2c12cd696f54183b37f82b8ba5
Sourabh Kumar <skumardas267@gmail.com>
2022-01-05 12:36:29 +0530

    Create overlapping_rectangles.cpp

---
+++ Goldman Sachs/overlapping_rectangles.cpp
@@ -0,0 +1,13 @@
+// checking for the horizontal alignment,checking of the vertial alignment. if any point merge return false.
+// else return true :)
+
+class Solution {
+  public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+         if(L1[0]>R2[0]||L2[0]>R1[0])
+           return false;
+       if(R1[1]>L2[1]||R2[1]>L1[1])
+           return false;
+       return true;
+    }
+};
