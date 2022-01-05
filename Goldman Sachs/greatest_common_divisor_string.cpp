2613efaeec13737b25ebabf83ee349af3d566441
Sourabh Kumar <skumardas267@gmail.com>
2022-01-05 12:52:53 +0530

    Create greatest_common_divisor_string.cpp

---
+++ Goldman Sachs/greatest_common_divisor_string.cpp
@@ -0,0 +1,9 @@
+// this is a simple method of finding if a string is a HCF of another string.
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+    if(str1+str2 != str2+str1) return "";
+    return str1.substr(0,gcd(str1.length(),str2.length()));
+    }
+};
